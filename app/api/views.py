6b11f95a57dd7951e5263e26c258e40248b1774c
Gui Talarico <gtalarico@gmail.com>
2017-11-07 14:17:27 -0500

    Fixed Dist gitignore

--- app/api/views.py
+++ app/api/views.py
@@ -11,15 +11,3 @@ from app.api import api_bp
 @api_bp.route('/')
 def api():
     return render_template('api.html', msg='API Blueprint View')
-
-
-from flask import session, jsonify
-
-@api_bp.route('/test')
-def test():
-    session['test_cookie'] = 'xxx'
-    return jsonify({'test': True})
-
-@api_bp.route('/testcookie')
-def testcookie():
-    return jsonify({'msg': 'Here is  cookie is {}'.format(session)})
