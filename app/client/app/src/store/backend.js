c880446ab0b0e35f934818f6b925afb32c7f38ce
gtalarico <gtalarico@gmail.com>
2018-04-21 02:14:22 -0400

    renamed axios

--- app/client/app/src/store/backend.js
+++ app/client/app/src/store/backend.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 
-let $backend = axios.create({
+let $axios = axios.create({
     baseURL: 'http://127.0.0.1:5000/api/',
     timeout: 5000,
     headers: {'Content-Type': 'application/json'}
 })
 
-$backend.interceptors.response.use(function (response) {
+$axios.interceptors.response.use(function (response) {
     return response
   }, function (error) {
     console.log(error)
@@ -17,12 +17,12 @@ $backend.interceptors.response.use(function (response) {
 export default {
 
   fetchResourceOne () {
-    return $backend.get(`resource/one`)
+    return $axios.get(`resource/one`)
       .then(response => response.data)
   },
 
   fetchResourceTwo (resourceId) {
-    return $backend.get(`resource/two/${resourceId}`)
+    return $axios.get(`resource/two/${resourceId}`)
       .then(response => response.data)
   }
 }
