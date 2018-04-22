ad0a733762448e1bd9ffbd6f78dcca417973cc6f
gtalarico <gtalarico@gmail.com>
2018-04-22 02:20:12 -0400

    Fix Flask Static Access

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ import click
 from app.api import api_rest, api_bp
 from app.client import client_bp
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
 app.register_blueprint(api_bp)
 app.register_blueprint(client_bp)
 
