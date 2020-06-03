5106acf6430e0d09d174d4f09109e903ee7d6128
AnkushSarkar10 <45667865+AnkushSarkar10@users.noreply.github.com>
2020-06-03 14:00:30 +0530

    Update vue.config.js (#28)

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api*': {
-        // Forward frontend dev server request for /api to django dev server
+        // Forward frontend dev server request for /api to flask dev server
         target: 'http://localhost:5000/'
       }
     }
